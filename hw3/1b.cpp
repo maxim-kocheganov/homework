4393f0745fa4f84cb5fe7d7f22139fa7556567dd
Maxim <maximkelasnce@users.noreply.github.com>
2015-05-19 00:24:23 +0300

    1 var 2 number

---
+++ hw3/1b.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct sPizza
+{
+	string Company;
+	float diam;
+	int weit;
+};
+int main()
+{
+	sPizza pizza;
+	getline(cin,pizza.Company);
+	cin >> pizza.diam;
+	cin >> pizza.weit;
+	cout << "Pizza: " << endl;
+	cout << "pizza.Company = " << pizza.Company << endl;
+	cout << "pizza.diam = " << pizza.diam << endl;
+	cout << "pizza.weit = " << pizza.weit << endl;
+	return 0;
+}
